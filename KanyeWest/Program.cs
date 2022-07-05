c1adbe3587378d942b7c714ac5d52ea140fe50e4
David Taccetta <taccetta14@gmail.com>
2022-07-05 15:13:27 -0400

    Initial KanyeWest Commit

---
+++ KanyeWest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeWest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var kanyeURL = "https://api.kanye.rest/";      
+
+            var client2 = new HttpClient();
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";            
+
+            for (int i = 0; i < 5 ; i++ )
+            {
+                var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+                var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+                
+                var ronResponse = client2.GetStringAsync(ronURL).Result;
+
+                var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+                
+                Console.WriteLine($"Ye says: {kanyeQuote}");
+                Console.WriteLine($"Ron says: {ronQuote}");
+            }
+        }
+    }
+}
